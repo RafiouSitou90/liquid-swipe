0ab52cc57b87d19459746ea5e45da67a5a92ccb6
Rafiou Sitou <rafiousitou90@gmail.com>
2021-06-25 00:24:57 -0300

    Add the Slider.tsx component Gesture

--- src/LiquidSwipe/Slider.tsx
+++ src/LiquidSwipe/Slider.tsx
@@ -1,45 +1,133 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import React, { useEffect } from "react";
+import { StyleSheet, View } from "react-native";
 
-import Wave, { HEIGHT, MARGIN_WIDTH, Side, WIDTH } from './Wave'
-import Button from './Button'
+import Wave, { MARGIN_WIDTH, Side, WIDTH } from "./Wave";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+  runOnJS,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import { snapPoint, useVector } from "react-native-redash";
 
-const PREV = WIDTH
-const NEXT = 0
+const PREV = WIDTH;
+const NEXT = 0;
 
 interface SliderProps {
-	index: number
-	setIndex: (value: number) => void
-	children: JSX.Element
-	prev?: JSX.Element
-	next?: JSX.Element
+  index: number;
+  setIndex: (value: number) => void;
+  children: JSX.Element;
+  prev?: JSX.Element;
+  next?: JSX.Element;
 }
 
 const Slider = ({
-	index,
-	children: current,
-	prev,
-	next,
-	setIndex,
+  index,
+  children: current,
+  prev,
+  next,
+  setIndex,
 }: SliderProps) => {
-	const hasPrev = !!prev
-	const hasNext = !!next
+  const hasPrev = !!prev;
+  const hasNext = !!next;
+  const activeSide = useSharedValue(Side.NONE);
+  const isTransitioningLeft = useSharedValue<boolean>(false);
+  const isTransitioningRight = useSharedValue<boolean>(false);
+  const left = useVector();
+  const right = useVector();
 
-	return (
-		<View style={StyleSheet.absoluteFill}>
-			{current}
-			{prev && (
-				<View style={[StyleSheet.absoluteFill]}>
-					<Wave side={Side.LEFT}>{prev}</Wave>
-				</View>
-			)}
-			{next && (
-				<View style={StyleSheet.absoluteFill}>
-					<Wave side={Side.RIGHT}>{next}</Wave>
-				</View>
-			)}
-		</View>
-	)
-}
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart: ({ x }) => {
+      if (x < MARGIN_WIDTH) {
+        activeSide.value = Side.LEFT;
+      } else if (x > WIDTH - MARGIN_WIDTH) {
+        activeSide.value = Side.RIGHT;
+      } else {
+        activeSide.value = Side.NONE;
+      }
+    },
+    onActive: ({ x, y }) => {
+      if (activeSide.value === Side.LEFT) {
+        left.x.value = x;
+        left.y.value = y;
+      } else if (activeSide.value === Side.RIGHT) {
+        left.x.value = WIDTH - x;
+        right.y.value = y;
+      }
+    },
+    onEnd: ({ x, velocityX, velocityY }) => {
+      if (activeSide.value === Side.LEFT) {
+        const snapPoints = [MARGIN_WIDTH, WIDTH];
+        const dest = snapPoint(x, velocityX, snapPoints);
+        isTransitioningLeft.value = dest === WIDTH;
+        left.x.value = withSpring(
+          dest,
+          {
+            velocity: velocityX,
+            overshootClamping: isTransitioningLeft.value,
+            restSpeedThreshold: isTransitioningLeft.value ? 100 : 0.01,
+            restDisplacementThreshold: isTransitioningLeft.value ? 100 : 0.01,
+          },
+          () => {
+            if (isTransitioningLeft.value) {
+              runOnJS(setIndex)(index - 1);
+            }
+          }
+        );
+      } else if (activeSide.value === Side.RIGHT) {
+        const snapPoints = [WIDTH - MARGIN_WIDTH, 0];
+        const dest = snapPoint(x, velocityX, snapPoints);
+        isTransitioningRight.value = dest === 0;
+        right.x.value = withSpring(
+          WIDTH - dest,
+          {
+            velocity: velocityX,
+            overshootClamping: isTransitioningRight.value,
+            restSpeedThreshold: isTransitioningRight.value ? 100 : 0.01,
+            restDisplacementThreshold: isTransitioningRight.value ? 100 : 0.01,
+          },
+          () => {
+            if (isTransitioningRight.value) {
+              runOnJS(setIndex)(index + 1);
+            }
+          }
+        );
+      }
+    },
+  });
 
-export default Slider
+  useEffect(() => {
+    left.x.value = withSpring(MARGIN_WIDTH);
+    right.x.value = withSpring(MARGIN_WIDTH);
+  }, [left.x, right.x]);
+
+  const leftStyle = useAnimatedStyle(() => ({
+    zIndex: activeSide.value === Side.LEFT ? 100 : 0,
+  }));
+
+  return (
+    <PanGestureHandler onGestureEvent={onGestureEvent}>
+      <Animated.View style={StyleSheet.absoluteFill}>
+        {current}
+        {prev && (
+          <Animated.View style={[StyleSheet.absoluteFill, leftStyle]}>
+            <Wave side={Side.LEFT} position={left}>
+              {prev}
+            </Wave>
+          </Animated.View>
+        )}
+        {next && (
+          <View style={StyleSheet.absoluteFill}>
+            <Wave side={Side.RIGHT} position={right}>
+              {next}
+            </Wave>
+          </View>
+        )}
+      </Animated.View>
+    </PanGestureHandler>
+  );
+};
+
+export default Slider;
--- src/LiquidSwipe/Wave.tsx
+++ src/LiquidSwipe/Wave.tsx
@@ -1,47 +1,76 @@
-import React, { ReactNode } from 'react'
-import { Dimensions, StyleSheet, View } from 'react-native'
+import React, { ReactNode } from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
 import Animated, {
-	Extrapolate,
-	interpolate,
-	useAnimatedProps,
-	useDerivedValue,
-	withSpring,
-} from 'react-native-reanimated'
-import Svg, { Path } from 'react-native-svg'
-import MaskedView from '@react-native-community/masked-view'
-import { Vector } from 'react-native-redash'
+  Extrapolate,
+  interpolate,
+  useAnimatedProps,
+  useDerivedValue,
+  withSpring,
+} from "react-native-reanimated";
+import Svg, { Path } from "react-native-svg";
+import MaskedView from "@react-native-community/masked-view";
+import { Vector } from "react-native-redash";
 
-export const { width: WIDTH, height: HEIGHT } = Dimensions.get('screen')
-export const MIN_LEDGE = 25
-export const MARGIN_WIDTH = MIN_LEDGE + 50
+export const { width: WIDTH, height: HEIGHT } = Dimensions.get("screen");
+export const MIN_LEDGE = 25;
+export const MARGIN_WIDTH = MIN_LEDGE + 50;
 
 // 0.5522847498 is taken from https://spencermortensen.com/articles/bezier-circle/
-const C = 0.5522847498
+const C = 0.5522847498;
 
-const AnimatedPath = Animated.createAnimatedComponent(Path)
+const AnimatedPath = Animated.createAnimatedComponent(Path);
 
 const vec2 = (x: number, y: number) => {
-	'worklet'
-	return { x, y }
-}
+  "worklet";
+  return { x, y };
+};
 const curve = (c1: Vector, c2: Vector, to: Vector) => {
-	'worklet'
-	return `C ${c1.x} ${c1.y} ${c2.x} ${c2.y} ${to.x} ${to.y}`
-}
+  "worklet";
+  return `C ${c1.x} ${c1.y} ${c2.x} ${c2.y} ${to.x} ${to.y}`;
+};
 
 export enum Side {
-	LEFT,
-	RIGHT,
-	NONE,
+  LEFT,
+  RIGHT,
+  NONE,
 }
 
 interface WaveProps {
-	side: Side
-	children: ReactNode
+  side: Side;
+  children: ReactNode;
+  position: Vector<Animated.SharedValue<number>>;
 }
+const Wave = ({ side, children, position }: WaveProps) => {
+  const animatedProps = useAnimatedProps(() => {
+    const d = ["M 0 0", `H ${position.x.value}`, `V ${HEIGHT}`, "H 0", "Z"];
 
-const Wave = ({ side, children }: WaveProps) => {
-	return <View style={StyleSheet.absoluteFill}>{children}</View>
-}
+    return {
+      d: d.join(" "),
+    };
+  });
+  return (
+    <MaskedView
+      style={StyleSheet.absoluteFill}
+      maskElement={
+        <Svg
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              transform: [
+                {
+                  rotateY: side === Side.RIGHT ? "180deg" : "0deg",
+                },
+              ],
+            },
+          ]}
+        >
+          <AnimatedPath animatedProps={animatedProps} fill={"black"} />
+        </Svg>
+      }
+    >
+      {children}
+    </MaskedView>
+  );
+};
 
-export default Wave
+export default Wave;
