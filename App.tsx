26d7bc63ec401d58fdb1b1eba3711b36aa710faf
Rafiou Sitou <rafiousitou90@gmail.com>
2021-06-24 22:58:55 -0300

    Add components:
    - Button.tsx
    - Slide.tsx
    - Slider.tsx
    - Wave.tsx

--- App.tsx
+++ App.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Routes } from './src/Routes'
 import { LoadAssets } from './src/components'
-import LiquidSwipe from './src/LiquidSwipe'
+import LiquidSwipe, { assets as liquidSwipeAssets } from './src/LiquidSwipe'
 
-const assets: number[] | undefined = []
+const fonts = {
+	'SFProDisplay-Bold': require('./src/assets/fonts/SFPro/SF-Pro-Display-Bold.otf'),
+	'SFProDisplay-Semibold': require('./src/assets/fonts/SFPro/SF-Pro-Display-Semibold.otf'),
+	'SFProDisplay-Regular': require('./src/assets/fonts/SFPro/SF-Pro-Display-Regular.otf'),
+	'SFProDisplay-Medium': require('./src/assets/fonts/SFPro/SF-Pro-Display-Medium.otf'),
+}
+
+const assets: number[] | undefined = [...liquidSwipeAssets]
 
 const { Navigator, Screen } = createStackNavigator<Routes>()
 
@@ -24,7 +31,7 @@ const AppNavigator = () => (
 
 const App = () => {
 	return (
-		<LoadAssets assets={assets}>
+		<LoadAssets assets={assets} fonts={fonts}>
 			<AppNavigator />
 		</LoadAssets>
 	)
